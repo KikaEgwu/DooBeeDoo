12452b60a9d0a820d121d8959061f0161e172b8f
Anthony Egwu <kaegwu@gmail.com>
2017-04-24 12:02:24 -0500

    initial commit

---
+++ apps/exam/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+# Create your views here.
+def index(request):
+	return render(request, 'exam/index.html')
